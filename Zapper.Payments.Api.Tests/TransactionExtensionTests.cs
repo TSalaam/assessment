fe073e9db3ddb046f7dc28bfc1b615de39856e38
Thaabiet Salaam <thaabiet74@gmail.com>
2021-03-30 15:30:04 +0200

    Task 3 - Fix the failing unit test(s)

--- Zapper.Payments.Api.Tests/TransactionExtensionTests.cs
+++ Zapper.Payments.Api.Tests/TransactionExtensionTests.cs
@@ -1,32 +1,31 @@
 using NUnit.Framework;
+
 using Zapper.Payment.Api.Extensions;
 using Zapper.Payment.Api.Models;
 
-namespace Zapper.Payments.Api.Tests
-{
-    public class Tests
-    {
+namespace Zapper.Payments.Api.Tests {
+
+    public class Tests {
+
         [Test]
-        public void TransactionExtension_TipAmount_Is_Zero_If_Tip_Percent_Null()
-        {
-            var transaction = new Transaction
-            {
+        public void TransactionExtension_TipAmount_Is_Zero_If_Tip_Percent_Null() {
+
+            var transaction = new Transaction {
                 Amount = 100
             };
 
-            Assert.AreEqual(transaction.CalculateTipTotal(), 0);
+            Assert.AreEqual(0, transaction.CalculateTipTotal());
         }
 
         [Test]
-        public void TransactionExtension_TipAmount_Calculates_Correctly_For_Percentage()
-        {
-            var transaction = new Transaction
-            {
-                Amount = 155,
+        public void TransactionExtension_TipAmount_Calculates_Correctly_For_Percentage() {
+
+            var transaction = new Transaction {
+                Amount = 160,
                 TipPercentage = 10
             };
 
-            Assert.AreEqual(transaction.CalculateTipTotal(), 16);
+            Assert.AreEqual(16, transaction.CalculateTipTotal());
         }
     }
 }
